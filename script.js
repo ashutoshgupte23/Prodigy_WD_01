fec58b870d1f40b1bbd5822e988f6c21498c17e7
ashutoshgupte23 <ashutoshofficial39@gmail.com>
2025-02-19 07:19:58 +0530

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
-// Change navigation menu style on scroll
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    header.classList.toggle('scroll-active', window.scrollY > 0);
-});
 
-// Add to cart functionality
-const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const accordion_containers = document.querySelectorAll(".accordion-container");
 
-addToCartButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        const itemName = button.dataset.item;
-        const itemPrice = parseFloat(button.dataset.price);
-        addToCart(itemName, itemPrice);
-    });
-});
-function addToCart(name, price) {
-    console.log(`Added ${name} to cart for $${price}`);
-}
-document.querySelectorAll('.add-to-cart').forEach(button => {
-    button.addEventListener('click', function() {
-        const itemName = this.getAttribute('data-item');
-        const itemPrice = parseFloat(this.getAttribute('data-price'));
-
-        alert(`Added "${itemName}" to cart for Rs:${itemPrice}/-`);
-    });
-});
-document.getElementById('form-contact').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-    this.reset();
+accordion_containers.forEach((accordion_container) => {
+  accordion_container.addEventListener("click", function () {
+    const data = this.nextElementSibling; // get data
+    data.classList.toggle("show");
+    const icon = this.querySelector("i");
+    icon.classList.toggle("animate");
+  });
 });
